e514927b58132b5991db1aac6ace226f7114efa8
chanduka99 <chanduka404@gmail.com>
2025-01-30 16:38:23 +0530

    test .gitignore config

--- terraform/aws-api-gateway.tf
+++ terraform/aws-api-gateway.tf
@@ -1,45 +1,13 @@
-resource "aws_api_gateway_rest_api" "example" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "example"
-      version = "1.0"
-    }
-    paths = {
-      "/path1" = {
-        get = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "GET"
-            payloadFormatVersion = "1.0"
-            type                 = "HTTP_PROXY"
-            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
-          }
-        }
-      }
-    }
-  })
-
-  name = "example"
-
+resource "aws_api_gateway_rest_api" "Test" {
+  name        = "TestAPI"
+  description = "This is my API for demonstration purposes"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  stage_name    = "example"
+resource "aws_api_gateway_resource" "Root" {
+  rest_api_id = aws_api_gateway_rest_api.Test.id
+  parent_id   = aws_api_gateway_rest_api.Test.root_resource_id
+  path_part   = "normal-priority"
 }
